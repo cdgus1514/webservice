60f21e1c9282d3d15ddffc05caf34dbdf60bed9b
dhchoi <cdgus1514@gmail.com>
2023-08-31 15:38:44 +0900

    JPA 추가

---
+++ src/test/java/dh/example/webservice/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,51 @@
+package dh.example.webservice.domain.posts;
+
+import org.assertj.core.api.Assertions;
+import org.junit.After;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class PostsRepositoryTest {
+
+    @Autowired PostsRepository postsRepository;
+
+
+    @After
+    public void clean() {
+        postsRepository.deleteAll();
+    }
+
+
+    @Test
+    public void 게시글테스트() {
+
+        // given
+        String title = "테스트 게시글";
+        String content = "테스트 본문";
+
+        postsRepository.save(
+                Posts.builder()
+                        .title(title)
+                        .content(content)
+                        .author("cdgus1514@gmail.com")
+                        .build());
+
+        // when
+        List<Posts> postsList = postsRepository.findAll();
+
+        // then
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+    }
+
+
+
+}
