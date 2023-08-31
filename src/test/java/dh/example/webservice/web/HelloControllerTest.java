73368485ced8735a362eeaf58844851ac8e22fc9
dhchoi <cdgus1514@gmail.com>
2023-08-31 14:51:51 +0900

    테스트코드 추가

---
+++ src/test/java/dh/example/webservice/web/HelloControllerTest.java
@@ -0,0 +1,42 @@
+package dh.example.webservice.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(controllers = HelloController.class)
+class HelloControllerTest {
+
+    @Autowired private MockMvc mockMvc;
+
+
+    @Test
+    public void hello_테스트() throws Exception {
+        String hello = "hello";
+
+        mockMvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(hello));
+    }
+
+
+    @Test
+    public void hello_dto_테스트() throws Exception {
+        String name = "hello";
+        int amount = 1000;
+
+        mockMvc.perform(get("/hello/dto")
+                        .param("name", name)
+                        .param("amount", String.valueOf(amount)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is(name)))
+                .andExpect(jsonPath("$.amount", is(amount)));
+    }
+
+}
