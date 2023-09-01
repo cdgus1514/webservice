3b236234da86df72ffc58150091c6ce7ed5bf18f
dhchoi <cdgus1514@gmail.com>
2023-09-01 13:15:03 +0900

    컨트롤러/서비스 테스트코드 추가

---
+++ src/main/java/dh/example/webservice/service/posts/PostsService.java
@@ -0,0 +1,21 @@
+package dh.example.webservice.service.posts;
+
+import dh.example.webservice.domain.posts.PostsRepository;
+import dh.example.webservice.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
---
+++ src/test/java/dh/example/webservice/web/PostsApiControllerTest.java
@@ -0,0 +1,65 @@
+package dh.example.webservice.web;
+
+import dh.example.webservice.domain.posts.Posts;
+import dh.example.webservice.domain.posts.PostsRepository;
+import dh.example.webservice.web.dto.PostsSaveRequestDto;
+import org.assertj.core.api.Assertions;
+import org.junit.After;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class PostsApiControllerTest {
+
+    @LocalServerPort private int port;
+
+    @Autowired private TestRestTemplate restTemplate;
+
+    @Autowired private PostsRepository postsRepository;
+
+
+    @After
+    public void tearDown() throws Exception {
+        postsRepository.deleteAll();
+    }
+
+
+    @Test
+    public void Posts_등록테스트() throws Exception {
+
+        // given
+        String title = "title";
+        String content = "content";
+        PostsSaveRequestDto requestDto = PostsSaveRequestDto.builder()
+                .title(title)
+                .content(content)
+                .author("author")
+                .build();
+
+        String url = "http://localhost:" + port + "/api/v1/posts";
+
+
+        // when
+        ResponseEntity<Long> responseEntity = restTemplate.postForEntity(url, requestDto, Long.class);
+
+
+        // then
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(responseEntity.getBody()).isGreaterThan(0L);
+
+        List<Posts> list = postsRepository.findAll();
+        assertThat(list.get(0).getTitle()).isEqualTo(title);
+        assertThat(list.get(0).getContent()).isEqualTo(content);
+    }
+
+}
