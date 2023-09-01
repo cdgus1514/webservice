31a295ba40b9b666b5f561e8832eebc747488e43
dhchoi <cdgus1514@gmail.com>
2023-09-01 13:55:34 +0900

    등록시간 자동화 및 테스트코드 추가

--- src/main/java/dh/example/webservice/domain/posts/Posts.java
+++ src/main/java/dh/example/webservice/domain/posts/Posts.java
@@ -1,5 +1,6 @@
 package dh.example.webservice.domain.posts;
 
+import dh.example.webservice.domain.BaseTimeEntity;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,7 +10,7 @@ import javax.persistence.*;
 @Getter
 @Entity
 @NoArgsConstructor
-public class Posts {
+public class Posts extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/test/java/dh/example/webservice/domain/posts/PostsRepositoryTest.java
+++ src/test/java/dh/example/webservice/domain/posts/PostsRepositoryTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -46,6 +47,30 @@ class PostsRepositoryTest {
         assertThat(posts.getContent()).isEqualTo(content);
     }
 
+
+    @Test
+    public void BaseTimeEntity_테스트() {
+
+        // given
+        LocalDateTime now = LocalDateTime.of(2023,9,1,0,0,0);
+        postsRepository.save(Posts.builder()
+                .title("title")
+                .content("content")
+                .author("author").build());
+
+
+        // when
+        List<Posts> list = postsRepository.findAll();
+
+
+        // then
+        Posts posts = list.get(0);
+
+        System.out.println("createdDate = " + posts.getCreatedDate() + ", modifiedDate = " + posts.getModifiedDate());
+
+        assertThat(posts.getCreatedDate()).isAfter(now);
+        assertThat(posts.getModifiedDate()).isAfter(now);
+    }
 
 
 }
