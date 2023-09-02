ca6f9cd39232253e1be5a7ae18aea0ef100e19db
dhchoi <cdgus1514@gmail.com>
2023-09-02 20:12:49 +0900

    네이버, 카카오 로그인 추가

--- src/main/java/dh/example/webservice/config/auth/dto/OAuthAttributes.java
+++ src/main/java/dh/example/webservice/config/auth/dto/OAuthAttributes.java
@@ -28,6 +28,14 @@ public class OAuthAttributes {
 
 
     public static OAuthAttributes of (String registrationId, String userNameAttributeName, Map<String, Object> attributes) {
+
+        if("naver".equals(registrationId)) {
+            return ofNaver("id", attributes);
+        }
+        if("kakao".equals(registrationId)) {
+            return ofKakao("id", attributes);
+        }
+
         return ofGoogle(userNameAttributeName, attributes);
     }
 
@@ -36,6 +44,31 @@ public class OAuthAttributes {
                 .name(String.valueOf(attributes.get("name")))
                 .email(String.valueOf(attributes.get("email")))
                 .picture(String.valueOf(attributes.get("picture")))
+                .attributes(attributes)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
+    }
+
+    private static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes) {
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+
+        return OAuthAttributes.builder()
+                .name(String.valueOf(response.get("name")))
+                .email(String.valueOf(response.get("email")))
+                .picture(String.valueOf(response.get("profile_image")))
+                .attributes(response)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
+    }
+
+    private static OAuthAttributes ofKakao(String userNameAttributeName, Map<String, Object> attributes) {
+        Map<String, Object> account = (Map<String, Object>) attributes.get("kakao_account");
+        Map<String, Object> profile = (Map<String, Object>) account.get("profile");
+
+        return OAuthAttributes.builder()
+                .name(String.valueOf(profile.get("nickname")))
+                .email(String.valueOf(account.get("email")))
+                .picture(String.valueOf(profile.get("profile_image_url")))
                 .attributes(attributes)
                 .nameAttributeKey(userNameAttributeName)
                 .build();
