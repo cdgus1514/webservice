911105a3ea99baba8c96836290f9b10c4c89112c
dhchoi <cdgus1514@gmail.com>
2023-09-18 13:16:47 +0900

    nginx 설정추가 및 테스트

---
+++ src/test/java/dh/example/webservice/web/ProfileControllerTest.java
@@ -0,0 +1,30 @@
+package dh.example.webservice.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class ProfileControllerTest {
+
+    @LocalServerPort private int port;
+    @Autowired TestRestTemplate restTemplate;
+
+
+    @Test
+    public void profile_인증없이_호출() throws Exception {
+
+        String expected = "default";
+
+        ResponseEntity<String> response = restTemplate.getForEntity("/profile", String.class);
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(response.getBody()).isEqualTo(expected);
+    }
+}
---
+++ src/test/java/dh/example/webservice/web/ProfileControllerUnitTest.java
@@ -0,0 +1,67 @@
+package dh.example.webservice.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.mock.env.MockEnvironment;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ProfileControllerUnitTest {
+
+    @Test
+    public void real_profile조회() {
+
+        // given
+        String expectedProfile = "real";
+        MockEnvironment env = new MockEnvironment();
+
+        env.addActiveProfile(expectedProfile);
+        env.addActiveProfile("oauth");
+        env.addActiveProfile("real_db");
+
+        ProfileController controller = new ProfileController(env);
+
+        // when
+        String profile = controller.profile();
+
+        // then
+        assertThat(profile).isEqualTo(expectedProfile);
+    }
+
+
+    @Test
+    public void real_profile없으면_첫번째_조회() {
+
+        // given
+        String expectedProfile = "oauth";
+        MockEnvironment env = new MockEnvironment();
+
+        env.addActiveProfile(expectedProfile);
+        env.addActiveProfile("real-db");
+
+        ProfileController controller = new ProfileController(env);
+
+        // when
+        String profile = controller.profile();
+
+        // then
+        assertThat(profile).isEqualTo(expectedProfile);
+    }
+
+
+    @Test
+    public void active_profile없으면_default_조회() {
+
+        // given
+        String expectedProfile = "default";
+        MockEnvironment env = new MockEnvironment();
+
+        ProfileController controller = new ProfileController(env);
+
+        // when
+        String profile = controller.profile();
+
+        // then
+        assertThat(profile).isEqualTo(expectedProfile);
+    }
+
+}
